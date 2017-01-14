7990804c7ecf55ba1ff71e45e8563ef36d0af24e
CarmineDAlessandroHuman <carminedalessandro1@gmail.com>
2017-01-14 10:39:46 +0100

    proseguimento test cases

---
+++ src/CarrelloTest.java
@@ -0,0 +1,66 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import carrelloPackage.Carrello;
+import prodottipackage.Prodotto;
+
+public class CarrelloTest {
+	Carrello carrello;
+	ArrayList<Prodotto> prodotti;
+	@Before
+	public void setUp() throws Exception {
+		carrello = new Carrello ();
+		prodotti = new ArrayList <Prodotto>();
+		carrello.setId(4);
+		carrello.setLista(prodotti);
+		carrello.setPrezzo(5);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		carrello = null;
+	}
+
+	@Test
+	public void testSetPrezzo() {
+		carrello.setPrezzo(50);
+		assertEquals(50,carrello.getPrezzo(),0.001);
+	}
+
+	@Test
+	public void testGetPrezzo() {
+		assertEquals(5,carrello.getPrezzo(),0.01);
+	}
+
+	@Test
+	public void testGetLista() {
+		assertEquals(prodotti,carrello.getLista());
+	}
+
+	@Test
+	public void testSetLista() {
+		ArrayList <Prodotto> lista2 = new ArrayList <Prodotto> ();
+		carrello.setLista(lista2);
+		assertEquals(lista2,carrello.getLista());
+	}
+
+	@Test
+	public void testGetId() {
+		assertEquals(4,carrello.getId());
+	}
+
+	@Test
+	public void testSetId() {
+		carrello.setId(11);
+		assertEquals(11,carrello.getId());
+	}
+
+}
+
+
+}
